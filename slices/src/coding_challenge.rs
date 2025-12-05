a977ee28fc276f32877fb94c4cb5d501e99b0f78
Sivaprasad Chidambaram <csivaprasad.c@icloud.com>
2025-12-05 18:58:02 +0530

    Slices

--- slices/src/coding_challenge.rs
+++ slices/src/coding_challenge.rs
@@ -32,3 +32,33 @@ to the "Cocoa Puffs" String (in other words, a &String).
 Declare a `puffs` variable that extracts a slice of
 the text "Puffs" from the String. Print the slice.
 */
+
+pub fn coding_challenge() {
+    let mut cereals = [
+        String::from("Cookie Crisp"),
+        String::from("Cinnamon Toast Crunch"),
+        String::from("Frosted Flakes"),
+        String::from("Cocoa Puffs"),
+        String::from("Captain Crunch"),
+    ];
+
+    let first_two = &cereals[..2];
+    println!("First two cereals: {:?}", first_two);
+
+    let mid_three = &cereals[1..4];
+    println!("Middle three cereals: {:?}", mid_three);
+
+    let last_three = &mut cereals[2..];
+    println!("Last three cereals: {:?}", last_three);
+
+    last_three[2] = String::from("Lucky Charms");
+    println!("Updated cereals: {:?}", cereals);
+
+    let cookie_crisp = &cereals[0];
+    let cookie = &cookie_crisp[0..6];
+    println!("Slice of 'Cookie': {}", cookie);
+
+    let cocoa_puffs = &cereals[3];
+    let puffs = &cocoa_puffs[6..];
+    println!("Slice of 'Puffs': {}", puffs);
+}
--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,1 +1,62 @@
-fn main() {}
+mod coding_challenge;
+use crate::coding_challenge::*;
+
+fn main() {
+    let action_hero = String::from("Jason Statham");
+    let first_name = &action_hero[0..5];
+    println!("The action hero is: {}", first_name);
+
+    let last_name = &action_hero[6..];
+    println!("The last name is: {}", last_name);
+
+    let name = {
+        let my_action_hero = "Jason Statham";
+        &my_action_hero[0..5]
+    };
+    println!("{name}");
+
+    let food = "🍕";
+    println!("Food: {}", food.len());
+
+    do_hero_stuff(&action_hero);
+    let another_action_hero = "Sylvester Stallone";
+    do_hero_stuff(&another_action_hero);
+
+    // Array slices
+    let values = [4, 8, 15, 16, 23, 42];
+    let my_slice = &values[..4];
+    println!("Array slice: {:?}", my_slice);
+
+    let my_slice = &values[2..4];
+    println!("Array slice: {:?}", my_slice);
+
+    let my_slice = &values[2..];
+    println!("Array slice: {:?}", my_slice);
+    print!("Length of the array slice:");
+    print_length(my_slice);
+
+    let my_slice = &values[..];
+    println!("Array slice: {:?}", my_slice);
+
+    let my_slice = &values;
+    println!("Array slice: {:?}", my_slice);
+    print!("Length of the array slice:");
+    print_length(my_slice);
+
+    let mut my_array = [10, 15, 20, 25, 30];
+    let slice = &mut my_array[2..4];
+    println!("My slice: {:?}", slice);
+    slice[0] = 100;
+    println!("My slice: {:?}", slice);
+    println!("My array: {:?}", my_array);
+
+    coding_challenge();
+}
+
+fn do_hero_stuff(hero_name: &str) {
+    println!("Doing stuff with hero: {}", hero_name);
+}
+
+fn print_length(reference: &[i32]) {
+    println!("{}", reference.len());
+}
